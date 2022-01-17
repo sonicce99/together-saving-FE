85d7814678e7051ee6c2a86a65201465f68c13e9
bxxmi <bmi6638@gmail.com>
2022-01-17 21:50:31 +0900

    style: modify app.jsx logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import GlobalStyles from "./styles/globalStyle";
 
 const App = () => {
-  return (
-    <>
-      <GlobalStyles />
-    </>
-  );
+  return <GlobalStyles />;
 };
 
 export default App;
