fda49c1e76ed2dfea3ed8487b0bce1966b694d9b
bxxmi <bmi6638@gmail.com>
2022-01-18 15:15:39 +0900

    config: add views directory, files

---
+++ src/views/challenge/ChallengeViews.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChallengeViews = () => {
+  return <div></div>;
+};
+
+export default ChallengeViews;
---
+++ src/views/main/MainViews.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainViews = () => {
+  return <div></div>;
+};
+
+export default MainViews;
