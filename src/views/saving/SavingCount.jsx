b4fa463554baeeffb6dea4f02acb7d6c49dd2a84
bxxmi <bmi6638@gmail.com>
2022-01-19 12:41:44 +0900

    style: saving count ui desgin

--- src/views/saving/SavingCount.jsx
+++ src/views/saving/SavingCount.jsx
@@ -1,26 +1,55 @@
 import React from "react";
 import styled from "styled-components";
+import success from "../../images/save_success.png";
+import fail from "../../images/save_fail.png";
+import remain from "../../images/save_remain.png";
 
 const SavingCount = () => {
   return (
-    <CountContainer>
+    <CountInfoList>
       <CountInfo>
-        <img src="" alt="success" />
-        <Info>저축성공</Info>
-        <Info>3번</Info>
+        <Img src={success} alt="success" />
+        <Text>저축성공</Text>
+        <Text>3번</Text>
       </CountInfo>
-    </CountContainer>
+      <CountInfo>
+        <Img src={fail} alt="fail" />
+        <Text>저축실패</Text>
+        <Text>3번</Text>
+      </CountInfo>
+      <CountInfo>
+        <Img src={remain} alt="remain" />
+        <Text>남은저축</Text>
+        <Text>13번</Text>
+      </CountInfo>
+    </CountInfoList>
   );
 };
 
-const CountContainer = styled.div`
-  background-color: wheat;
-`;
-
-const CountInfo = styled.div`
+const CountInfoList = styled.ul`
   display: flex;
+  flex-direction: column;
 `;
 
-const Info = styled.p``;
+const CountInfo = styled.li`
+  display: flex;
+  margin-bottom: 10px;
+`;
+
+const Img = styled.img`
+  width: 16px;
+  height: 16px;
+  margin-right: 16px;
+`;
+
+const Text = styled.p`
+  color: ${({ theme }) => theme.colors.colorGray3};
+  font-size: ${({ theme }) => theme.fontSize.fontXSmall};
+  font-weight: ${({ theme }) => theme.fontWeights.weightNormal};
+
+  &:first-of-type {
+    flex-grow: 1;
+  }
+`;
 
 export default SavingCount;
