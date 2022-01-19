b9b4d4a1e140599138cd414e28db63416ff225e5
bxxmi <bmi6638@gmail.com>
2022-01-19 12:21:03 +0900

    config: add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.(png|jpg)$/,
+        use: ["file-loader"],
+      },
     ],
   },
   resolve: {
