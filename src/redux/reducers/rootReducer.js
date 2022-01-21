e1e692e1455e4e320363cd7925342d12bf840746
bxxmi <bmi6638@gmail.com>
2022-01-21 14:53:30 +0900

    feat: combine reducers

---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import savingInfoReducer from "./savingInfoReducer";
+
+const reducer = combineReducers({
+  savingInfoReducer,
+});
+
+export default reducer;
