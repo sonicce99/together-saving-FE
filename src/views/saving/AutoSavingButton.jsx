498c4d0a1322bd47fde256e1e078b2e2f2c0dc48
bxxmi <bmi6638@gmail.com>
2022-01-25 15:20:27 +0900

    feat: use axios put method

--- src/views/saving/AutoSavingButton.jsx
+++ src/views/saving/AutoSavingButton.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
-const AutoSavingButton = () => {
-  const [isAutoSave, setIsAutoSave] = useState(false);
+const AutoSavingButton = ({ isAuto }) => {
+  const [isAutoSave, setIsAutoSave] = useState(isAuto);
 
   const handleSwitch = () => {
     setIsAutoSave(!isAutoSave);
   };
+
+  // 나중에 처리 put 404 error
+  // axios
+  //   .put("../../modules/autoSaving.json", { isAutoSave })
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((e) => console.log(e));
 
   return (
     <AutoSavingContainer>
