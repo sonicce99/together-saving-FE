2c053b6bccdb179d2ebdcd9deb9c584bf4c65f2f
dongsu <sonicce99@naver.com>
2022-01-26 18:03:43 +0900

    feat : New Route Create /challenge/success path

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/main/Main";
-import Challenge from "./pages/challenge/Challenge";
+import ChallengeContainer from "./pages/challenge/ChallengeContainer";
 import Saving from "./pages/saving/Saving";
 import styled from "styled-components";
 import SavingDeposit from "./pages/saving/SavingDeposit";
 import SavingSuccess from "./views/deposit/SavingSuccess";
 import ChallengePayment from "./pages/challenge/ChallengePayment";
+import ChallengePaymentSuccess from "./pages/challenge/ChallengePaymentSuccess";
 
 const App = () => {
   return (
     <Wrapper>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/challenge/:id" element={<Challenge />} />
+        <Route path="/challenge/:id" element={<ChallengeContainer />} />
         <Route path="/challenge/payment" element={<ChallengePayment />} />
+        <Route
+          path="/challenge/success"
+          element={<ChallengePaymentSuccess />}
+        />
         <Route path="/saving" element={<Saving />} />
         <Route path="/deposit" element={<SavingDeposit />} />
         <Route path="/success" element={<SavingSuccess />} />
