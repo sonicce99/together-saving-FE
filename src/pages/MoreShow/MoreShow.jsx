eafc1c38eb60f552324fcef8030256683bb84b0d
dongsu <sonicce99@naver.com>
2022-01-27 19:14:05 +0900

    feat : MoreShow component created

---
+++ src/pages/MoreShow/MoreShow.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import MoreShowTemplate from "../../views/MoreShow/MoreShowTemplate";
+import BottomNav from "../../components/BottomNav";
+
+const MoreShow = () => {
+  return (
+    <>
+      <MoreShowTemplate />
+      <BottomNav />
+    </>
+  );
+};
+
+export default MoreShow;
