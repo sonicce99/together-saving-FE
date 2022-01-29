ebf9badb092e5bf2b9c3ece67e54c8430568a26e
bxxmi <bmi6638@gmail.com>
2022-01-29 17:41:23 +0900

    feat: add infinite scroll component

--- src/views/main/MainTabs.jsx
+++ src/views/main/MainTabs.jsx
@@ -6,10 +6,12 @@ import ChallengeTemplate2 from "../../components/ChallengeTemplate2.jsx";
 import DivisionLine from "../../components/DivisionLine.jsx";
 import ChallengeCreateAndEct from "./ChallengeCreateAndEct.jsx";
 import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const MainTabs = () => {
   const [value, setValue] = React.useState(0); // Tabs 관련
 
+  const [dataLimit, setDataLimit] = React.useState(7);
   const [participatingChallenges, setParticipatingChallenges] = React.useState(
     []
   ); // 참여 중인 챌린지 Data
@@ -47,6 +49,10 @@ const MainTabs = () => {
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+  };
+
+  const handlePage = () => {
+    setPage(page + 1);
   };
 
   return (
@@ -87,10 +93,16 @@ const MainTabs = () => {
         </Box>
 
         <TabPanel value={value} index={0}>
-          <ChallengeTemplate1
-            title="참여중인 챌린지"
-            ChallengeArray={participatingChallenges}
-          />
+          <InfiniteScroll
+            dataLength={participatingChallenges.length}
+            next={handlePage}
+            hasMore={true}
+          >
+            <ChallengeTemplate1
+              title="참여중인 챌린지"
+              ChallengeArray={participatingChallenges}
+            />
+          </InfiniteScroll>
         </TabPanel>
 
         {value === 0 ? <DivisionLine /> : null}
