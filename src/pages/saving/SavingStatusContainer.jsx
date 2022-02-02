59dc30d9196cc8df8034f829755feaabfc6b7c39
bxxmi <bmi6638@gmail.com>
2022-02-02 20:19:53 +0900

    style: add skeleton ui

--- src/pages/saving/SavingStatusContainer.jsx
+++ src/pages/saving/SavingStatusContainer.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
+import styled from "styled-components";
 import SavingStatusInfo from "../../views/saving/SavingStatusInfo";
 import { useSelector, useDispatch } from "react-redux";
 import { getSavingInfo } from "../../redux/reducers/savingInfoReducer";
+import { Skeleton } from "@mui/material";
 
 const SavingStatusContainer = () => {
   const statusInfo = useSelector((state) => state.savingInfoReducer.savingInfo);
@@ -10,13 +12,43 @@ const SavingStatusContainer = () => {
 
   useEffect(() => {
     dispatch(getSavingInfo());
-  }, [dispatch]);
+  }, []);
 
-  if (statusInfo.loading) return <div>로딩중</div>;
+  if (statusInfo.loading) {
+    return (
+      <SkeletonContainer>
+        <Skeleton variant="circular" width={100} height={100} />
+        <Skeleton width="60%" height={60} />
+        <Skeleton width="100%" />
+        <Skeleton variant="rectangle" width="100%" height={100} />
+      </SkeletonContainer>
+    );
+  }
+
   if (statusInfo.error) return <div>에러 발생</div>;
   if (!statusInfo.data) return null;
 
   return <SavingStatusInfo savingStatus={statusInfo.data} />;
 };
+
+const SkeletonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid red;
+
+  &:nth-child(1) {
+    margin-bottom: 20px;
+  }
+
+  &:nth-child(2) {
+    margin-bottom: 42px;
+  }
+
+  &:nth-child(2) {
+    margin-bottom: 60px;
+  }
+`;
 
 export default SavingStatusContainer;
