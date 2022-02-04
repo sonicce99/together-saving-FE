a2660db7c02d64860dc2b79f8307a04c3c70cf8c
bxxmi <bmi6638@gmail.com>
2022-02-04 10:55:16 +0900

    config: add dotenv

--- src/utils/callApi.js
+++ src/utils/callApi.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// env로 빼자
-const API_ADDRESS = "http://183.99.247.17:8881/api/v1/users/challenges";
-
 // 챌린지 네임에 따라 다른 응답 보내주기
 export const connectChallengeApi = async (challengeName, dataPage) => {
   try {
@@ -14,7 +11,7 @@ export const connectChallengeApi = async (challengeName, dataPage) => {
         return participateData.data;
       case "popular": // 인기 더보기
         const popularData = await axios.get(
-          `http://183.99.247.17:8881/api/v1/auth/challenges?criteria=popularity&page=${dataPage}`
+          `${process.env.API_ADDRESS}/api/v1/auth/challenges?criteria=popularity&page=${dataPage}`
         );
         return popularData.data;
       default:
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.jsx",
@@ -26,6 +27,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new Dotenv({
+      path: "./.env",
     }),
   ],
   devServer: {
