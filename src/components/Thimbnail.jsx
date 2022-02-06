1910f616e476f2839545bf846a78736c8ef962c9
dongsu <sonicce99@naver.com>
2022-02-06 17:33:05 +0900

    feat : Time Component added

--- src/components/Thimbnail.jsx
+++ src/components/Thimbnail.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Time from "./Time";
 import styled from "styled-components";
 
 const Thumbnail = ({ thumbnail }) => {
-  return <Size>썸네일 이미지</Size>;
+  return (
+    <Size>
+      <Time />
+    </Size>
+  );
 };
 const Size = styled.div`
   width: ${({ theme }) => theme.viewSize.mobile};
