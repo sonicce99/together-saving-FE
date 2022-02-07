4ba242dc38084092029c964c45b1e1c50fc8d94e
dongsu <sonicce99@naver.com>
2022-02-07 20:35:59 +0900

    docs : useState remove

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import EventImg from "../images/EventImage.png";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Autoplay } from "swiper";
 import "swiper/css";
