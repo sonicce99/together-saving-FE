6a008b4aa6701bb4a4830675a9f0ab80c3f09ef7
bxxmi <bmi6638@gmail.com>
2022-02-08 15:02:43 +0900

    style: add skeleton ui

--- src/pages/saving/SavingRoomInfoContainer.jsx
+++ src/pages/saving/SavingRoomInfoContainer.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
+import styled from "styled-components";
 import SavingRoomInfo from "../../views/saving/SavingRoomInfo";
 import { useSelector, useDispatch } from "react-redux";
 import { getChallengesummaryInfo } from "../../redux/reducers/challengeSummaryReducer.js";
+import { Skeleton } from "@mui/material";
 
 const SavingRoomInfoContainer = ({ id }) => {
   const dispatch = useDispatch();
@@ -13,11 +15,36 @@ const SavingRoomInfoContainer = ({ id }) => {
     dispatch(getChallengesummaryInfo(id));
   }, []);
 
-  if (challengeInfo.loading) return <div>로딩중</div>;
+  if (challengeInfo.loading) {
+    return (
+      <SkeletonContainer>
+        <Skeleton variant="rectangle" width={92} height={70} />
+        <SkeletonTextContainer>
+          <Skeleton variant="text" width={230} height={40} />
+          <Skeleton variant="text" width={230} height={40} />
+        </SkeletonTextContainer>
+      </SkeletonContainer>
+    );
+  }
+
   if (challengeInfo.error) return <div>에러 발생</div>;
   if (!challengeInfo.data) return null;
 
   return <SavingRoomInfo challengeInfo={challengeInfo.data.data} />;
 };
+
+const SkeletonContainer = styled.div`
+  height: 97px;
+  padding: 14px 16px;
+  display: flex;
+`;
+
+const SkeletonTextContainer = styled.div`
+  margin-left: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default SavingRoomInfoContainer;
