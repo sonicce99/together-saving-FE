df6167b004380e5711cc4f453a806443184cc454
dongsu <sonicce99@naver.com>
2022-02-09 14:58:54 +0900

    fix : challenge_id props to SavingCommunication

--- src/pages/saving/SavingCommunicationContainer.jsx
+++ src/pages/saving/SavingCommunicationContainer.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import SavingCommunication from "../../views/saving/SavingCommunication";
 
 const SavingCommunicationContainer = () => {
-  return <SavingCommunication />;
+  // 챌린지 정보 가져오기
+  const challengeInfo = useSelector(
+    (state) => state.challengeSummaryReducer.challengeSummaryInfo
+  );
+
+  return (
+    <SavingCommunication challenge_id={challengeInfo.data.data.challenge_id} />
+  );
 };
 
 export default SavingCommunicationContainer;
