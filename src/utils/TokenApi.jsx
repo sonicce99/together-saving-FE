0ba2c4398bba0090befed6495ee20020917eebc0
dongsu <sonicce99@naver.com>
2022-02-10 16:09:34 +0900

    test

--- src/utils/TokenApi.jsx
+++ src/utils/TokenApi.jsx
@@ -4,6 +4,6 @@ import cookie from "react-cookies";
 const TOKEN = cookie.load("TOKEN");
 
 export const axiosInstance = axios.create({
-  baseURL: "/",
+  baseURL: "http://183.99.247.17:8881",
   headers: { Authorization: TOKEN },
 });
