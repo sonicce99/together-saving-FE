f480a35ee2ecf4af75ac68e65e79aca4d40b56e1
dongsu <sonicce99@naver.com>
2022-02-10 19:02:40 +0900

    test

---
+++ setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api", {
+      target: "http://183.99.247.17:8881/",
+      changeOrigin: true,
+    })
+  );
+};
