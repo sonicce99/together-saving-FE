5b3691ac933fa75f17eae3705510c8fd71e03df6
dongsu <sonicce99@naver.com>
2022-02-11 21:11:58 +0900

    docs : varible changed

--- src/views/saving/SavingCommunication.jsx
+++ src/views/saving/SavingCommunication.jsx
@@ -21,7 +21,7 @@ const SavingCommunication = ({ challenge_id }) => {
       setPage(0);
       setComments(array);
     } else {
-      if (data.data.comments.length === 0) setHasMore(false);
+      if (data.data.challenge_comments.length === 0) setHasMore(false);
       const array = [];
       array.push(data.data);
       setComments([...comments, ...array]);
@@ -35,6 +35,8 @@ const SavingCommunication = ({ challenge_id }) => {
   const handleScrollPage = () => {
     setPage((page) => page + 1);
   };
+
+  console.log(comments);
 
   const handlePostComments = async () => {
     await axiosInstance.post(
@@ -76,10 +78,10 @@ const SavingCommunication = ({ challenge_id }) => {
                   return (
                     <div key={i}>
                       <Date>{EachDay.date}</Date>
-                      {EachDay.comments.length === 0 ? (
+                      {EachDay.challenge_comments.length === 0 ? (
                         <Nocomments>더 이상 내용이 없습니다.</Nocomments>
                       ) : (
-                        EachDay.comments.map((EachComment, index) => {
+                        EachDay.challenge_comments.map((EachComment, index) => {
                           return EachComment.written_by_me ? ( // 내가 쓴 글
                             <Div3 key={index}>
                               <Me>{EachComment.content}</Me>
