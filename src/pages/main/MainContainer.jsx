33031f00dfe49f2eba91549822da82dd64256715
dongsu <sonicce99@naver.com>
2022-02-17 10:45:47 +0900

    refactor : Component name changed Main to MainContainer

--- src/pages/main/MainContainer.jsx
+++ src/pages/main/MainContainer.jsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Header from "../../components/Header.jsx";
 import Event from "../../components/Event.jsx";
 import MainTabs from "../../views/main/MainTabs.jsx";
 import BottomNav from "../../components/BottomNav.jsx";
-// import Time from "../../components/Time.jsx";
 
-const Main = () => {
+const MainContainer = () => {
   return (
     <>
-      {/* <Time /> */}
       <Header main />
       <MainContentContainer>
         <Event />
@@ -24,4 +22,4 @@ const MainContentContainer = styled.div`
   padding-top: 52px;
 `;
 
-export default Main;
+export default MainContainer;
