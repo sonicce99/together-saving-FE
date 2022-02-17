46d5a2aa84055e1d73a9b5db0cdc58a4e2a813b9
dongsu <sonicce99@naver.com>
2022-02-17 10:52:38 +0900

    refactor : comment added!

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,11 +1,13 @@
+// pages > main > Main.jsx
 import React from "react";
 import styled from "styled-components";
 // 해더 컴포넌트
 import Header from "../../components/Header.jsx";
 // Event Banner 컴포넌트
 import Event from "../../components/Event.jsx";
-// 메인 페이지
+// 메인 페이지 view 폴더 컴포넌트
 import MainTabs from "../../views/main/MainTabs.jsx";
+// bottom 네비게이션 컴포넌트
 import BottomNav from "../../components/BottomNav.jsx";
 
 const Main = () => {
